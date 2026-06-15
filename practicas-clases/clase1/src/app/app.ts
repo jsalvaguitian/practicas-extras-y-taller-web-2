d27c17b0f4a277e45f9454f2c4cdebfa46940136
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2026-06-15 18:42:44 -0300

    practica de dados solo front angula

--- practicas-clases/clase1/src/app/app.ts
+++ practicas-clases/clase1/src/app/app.ts
@@ -2,11 +2,43 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//asi se llama su selector que lo identifica cuando lo usamos en el index.html
   imports: [RouterOutlet],
-  templateUrl: './app.html',
+  templateUrl: './app.html',//este component typescript esta anidado al html
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('clase1');
+   
+  //Declaracion de variables
+  dadoIzquierdo : string = "dados/dice1.png";
+  dadoDerecho : string = "dados/dice4.png";
+
+  nro1:number = 1;
+  nro2:number = 2;
+
+  resultado = "";
+
+  lanzarDados(){
+    this.nro1 = Math.floor(Math.random()*5)+1;
+    this.nro2 = Math.floor(Math.random()*5)+1;
+
+    //console.log(this.nro1);
+    //console.log(this.nro2);
+
+    this.dadoIzquierdo = 'dados/dice'+ this.nro1 + ".png";
+    this.dadoDerecho = 'dados/dice' + this.nro2 + ".png";
+
+
+    if(this.nro1 == this.nro2){
+      this.resultado = "Ganaste :-)";
+    
+    }else{
+      this.resultado =  "Perdiste :-( sera para la proxima";
+    }
+
+    console.log(this.resultado);
+  }
+
+
 }
