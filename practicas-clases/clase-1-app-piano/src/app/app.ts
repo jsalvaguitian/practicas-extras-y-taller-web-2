d3ac76b69ed81ef39bfa8c5a947a3130c735b0ea
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2026-06-17 21:56:16 -0300

    ejercicio del pianito mi adolescencia practicando eventos, directivas e interpolacion

--- practicas-clases/clase-1-app-piano/src/app/app.ts
+++ practicas-clases/clase-1-app-piano/src/app/app.ts
@@ -8,5 +8,45 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+  teclado:any[]=[ //no es buena practic usar any
+    {
+      numeroSonido : 1,
+      color : "red"
+    },
+    {
+      numeroSonido : 2,
+      color : "burlywood"
+    },
+    {
+      numeroSonido : 3,
+      color : "violet"
+    },
+    {
+      numeroSonido : 4,
+      color : "pink"
+    },
+    {
+      numeroSonido : 5,
+      color : "palevioletred"
+    },
+    {
+      numeroSonido : 6,
+      color : "peru"
+    },
+    {
+      numeroSonido : 7,
+      color : "rosybrown"
+    },
+  ];
   protected readonly title = signal('clase-1-app-piano');
+
+  aplicarSonido(tecla:number){
+    const audio = new Audio();
+
+    audio.src='sonidos/note'+ tecla +'.mp3';
+    audio.volume = 0.2;
+    audio.load();
+    audio.play();
+  }
+
 }
